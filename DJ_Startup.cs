c84c2a859285e6318e9356fa981a9d664493a512
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-13 14:44:59 -0700

    gonna use cookies

--- DJ_Startup.cs
+++ DJ_Startup.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authentication.Cookies;
 using MySqlConnector;
 
 namespace Digital_Jungle_Blazor;
@@ -12,17 +13,19 @@ public class DJ_Startup
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
+        app.UseHttpsRedirection();
+        
         if (!env.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
             app.UseHsts();
         }
 
-        app.UseHttpsRedirection();
-
         app.UseStaticFiles();
 
         app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
 
         app.UseEndpoints(configure => {
             configure.MapBlazorHub();
@@ -34,6 +37,14 @@ public class DJ_Startup
     {
         services.AddRazorPages();
         services.AddServerSideBlazor();
+
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+            .AddCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+                options.SlidingExpiration = true;
+            });
+
         services.AddSingleton<Data.WeatherForecastService>();
         
         services.AddTransient<MySqlConnection>(_ => new MySqlConnection(_configuration["ConnectionStrings:Default"]));
