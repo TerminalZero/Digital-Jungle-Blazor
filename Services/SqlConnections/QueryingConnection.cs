5a4cc26cf1188623316513ccddd7f1e05ded4b22
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-18 02:45:02 -0700

    converted to functions

--- Services/SqlConnections/QueryingConnection.cs
+++ Services/SqlConnections/QueryingConnection.cs
@@ -2,9 +2,9 @@ namespace Digital_Jungle_Blazor.Services.SqlConnections;
 
 public class QueryingConnection {
     public IConfiguration _configuration;
-    public MySqlConnector.MySqlConnection Get
+    public MySqlConnector.MySqlConnection Get()
     {
-        get => new MySqlConnector.MySqlConnection(_configuration["ConnectionStrings:Querying"]);
+        return new MySqlConnector.MySqlConnection(_configuration["ConnectionStrings:Querying"]);
     }
     
     public QueryingConnection(IConfiguration configuration) => _configuration = configuration;
