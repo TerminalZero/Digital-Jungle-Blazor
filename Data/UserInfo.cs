a2655af1f37f5c4ffaf6d3267c55b7d62d7846fb
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-19 05:31:58 -0700

    name as non-null

--- Data/UserInfo.cs
+++ Data/UserInfo.cs
@@ -2,6 +2,6 @@ namespace Digital_Jungle_Blazor.Data;
 
 public class UserInfo {
     public int Id { get; set; }
-    public string? Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public DateTime JoinDate { get; set; }
 }
