266d032ce69b484183acc5c3ac64a61860941668
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-18 17:12:05 -0700

    folder restructure

--- DJ_Startup.cs
+++ DJ_Startup.cs
@@ -51,7 +51,7 @@ public class DJ_Startup
             }
         });
 
-        services.AddRazorPages(_ => _.RootDirectory = "/RazorPages");
+        services.AddRazorPages(_ => _.RootDirectory = "/BlazorFramework/RazorPages");
         services.AddServerSideBlazor();
 
         services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
