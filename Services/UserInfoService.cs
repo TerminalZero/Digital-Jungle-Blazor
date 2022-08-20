f150450285e2b5cde1deb08b3dd88dc1820e0b3f
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-20 00:21:09 -0700

    connection open check. sql indexing in-progam, inserts password into private table.

--- Services/UserInfoService.cs
+++ Services/UserInfoService.cs
@@ -9,17 +9,15 @@ public class UserInfoService {
     public UserInfoService(QueryingConnection qconnection, MasterConnection mconnection) {
         _qconnection = qconnection.Get();
         _mconnection = mconnection.Get();
-        _qconnection.Open();
-        _mconnection.Open();
     }
 
     public Task<DbTables.UserInfo> GetUserById(int id)
         => Task.FromResult(GetUsers(id - 1, id).Result.First());
     
     public async Task<List<DbTables.UserInfo>> GetUsers(int LIMIT_start = 0, int LIMIT_end = 1000) {
-        try {
+        if (_qconnection.State != System.Data.ConnectionState.Open) {
             await _qconnection.OpenAsync();
-        } catch {}
+        }
         
         using var command = new MySqlConnector.MySqlCommand($"SELECT * FROM UserInfo LIMIT { LIMIT_start }, { LIMIT_end };", _qconnection);
         using var reader = await command.ExecuteReaderAsync();
@@ -35,22 +33,19 @@ public class UserInfoService {
             userInfos.Add(userInfo);
         }
 
-        _qconnection.Close();
-
         return userInfos;
     }
 
     public async void PushUserInfo(DbTables.UserInfo userInfo) {
-        try {
+        if (_mconnection.State != System.Data.ConnectionState.Open) {
             await _mconnection.OpenAsync();
-        } catch {}
-        var command = new MySqlConnector.MySqlCommand($"INSERT INTO UserInfo(Name)" +
-            $"VALUE(\"{userInfo.Name}\");"
-        , _mconnection);
+        }
 
-        using var writer = command.ExecuteNonQueryAsync();
-        writer.Wait();
-        System.Console.WriteLine("2");
-
+        var command = new MySqlConnector.MySqlCommand(
+            $"INSERT INTO UserInfo(Id, Name)" +
+            $"SELECT COUNT(Id), \"{userInfo.Name}\" FROM UserInfo LIMIT 1;" +
+            $"INSERT INTO UserInfo_Private(Id, Password)" +
+            $"SELECT COUNT(Id), \"{userInfo.Password}\" FROM UserInfo LIMIT 1;"
+        , _mconnection).ExecuteNonQueryAsync();
     }
 }
