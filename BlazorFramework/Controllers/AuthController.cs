59731789aa92539a8aeee835315b07385ff6723c
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-21 00:23:50 -0700

    added auth controller as illustrated by syncfusion.com

---
+++ BlazorFramework/Controllers/AuthController.cs
@@ -0,0 +1,36 @@
+using Digital_Jungle_Blazor.Services;
+
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+[Route("/[controller]")]
+[ApiController]
+public class AuthController : ControllerBase
+{
+    UserInfoService _userInfoService;
+    public AuthController(UserInfoService userInfoService) {
+        _userInfoService = userInfoService;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login([FromForm] string name, [FromForm] string password)
+    {
+        var validatedUserInfo = await _userInfoService.ValidateUserInfo(name, password);
+        
+        if (validatedUserInfo != null) {
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity(new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, validatedUserInfo.Id.ToString()),
+                new Claim(ClaimTypes.NameIdentifier, name)
+            }, "auth");
+            ClaimsPrincipal claims = new ClaimsPrincipal(claimsIdentity);
+            await HttpContext.SignInAsync(claims);
+
+        }
+        return Redirect("/");
+    }
+}
