327b8ea1e425df42b095ed1657db3d69d20875c2
Michael Taylor <michaeltaylor4923@gmail.com>
2022-08-22 14:21:51 -0700

    commit

--- DbTables/UserInfo.cs
+++ DbTables/UserInfo.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Digital_Jungle_Blazor.DbTables;
 
 public class UserInfo {
     public int Id { get; set; }
+    [MinLength(4)]
     public string Name { get; set; } = string.Empty;
     public DateTime JoinDate { get; set; }
     public string Password { get; set; } = string.Empty;
